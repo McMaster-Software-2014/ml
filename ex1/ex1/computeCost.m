b1da15d5d73c29ffccd01601eaab9b3c1af057b0
Arkhaix <arkhaix@arkhaix.com>
2012-04-26 02:23:29 -0700

    [ml ex1] Renamed all the variables in computeCost.  I don't know how math people do it, but I don't speak greek.

--- ex1/ex1/computeCost.m
+++ ex1/ex1/computeCost.m
@@ -1,28 +1,27 @@
-function J = computeCost(X, y, theta)
+function J = computeCost(inputData, expectedResult, testParameters)
 %COMPUTECOST Compute cost for linear regression
 %   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
 %   parameter for linear regression to fit the data points in X and y
 
 % Initialize some useful values
-m = length(y); % number of training examples
-ratio = 1/(2*m);
+numInputs = length(inputData); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
+totalCost = 0;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
 
-for j = 1:m,
-%prediction = theta' * X(j);
-%distanceSquared = (sum(prediction) - y(j)).^2;
-prediction = theta(1,1) + (theta(2,1) * X(j,2));
-distanceSquared = (prediction - y(j))^2;
-J = J + (distanceSquared * ratio);
+for inputIndex = 1:numInputs,
+testResult = testParameters' * inputData(inputIndex);
+cost = (sum(testResult) - expectedResult(inputIndex)).^2;
+totalCost += cost;
 end;
 
+J = totalCost / (2*numInputs);
 
 % =========================================================================
 
